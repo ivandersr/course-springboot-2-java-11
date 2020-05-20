d99720e68c3bf8156a9a44b997db7c7ef5eb9294
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2020-05-20 13:30:39 -0300

    OrderStatus enum

--- src/main/java/com/salvadot/course/config/TestConfig.java
+++ src/main/java/com/salvadot/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.salvadot.course.entities.Order;
 import com.salvadot.course.entities.User;
+import com.salvadot.course.entities.enums.OrderStatus;
 import com.salvadot.course.repositories.OrderRepository;
 import com.salvadot.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2020-05-20T07:35:43Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-05-20T09:52:23Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2020-05-20T11:02:01Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2020-05-20T07:35:43Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-05-20T09:52:23Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2020-05-20T11:02:01Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));

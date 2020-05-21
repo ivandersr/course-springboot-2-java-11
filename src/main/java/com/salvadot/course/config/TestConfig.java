274d9781029e504df29d8bddbe251f38b1b38faa
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2020-05-21 10:00:59 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/salvadot/course/config/TestConfig.java
+++ src/main/java/com/salvadot/course/config/TestConfig.java
@@ -24,30 +24,30 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777777", "123456");
-		
+
 		Order o1 = new Order(null, Instant.parse("2020-05-20T07:35:43Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2020-05-20T09:52:23Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2020-05-20T11:02:01Z"), OrderStatus.WAITING_PAYMENT, u1);
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
-		
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante..", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
@@ -58,8 +58,17 @@ public class TestConfig implements CommandLineRunner {
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		System.out.println(p2.getCategories().size());
 	}
-	
-	
 
 }
